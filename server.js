29931e0525d8fe803d8137aaa78ee6d3af5b6b14
RakeshReddy21 <152692196+RakeshReddy21@users.noreply.github.com>
2025-12-07 22:23:36 +0530

    updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://aerchain-frontend-puce.vercel.app/',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
